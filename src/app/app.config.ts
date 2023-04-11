5d5efcc42bacc8f22fa43b4cabd285f2d736422a
jeffr63 <jeffr63@gmail.com>
2023-04-11 07:25:54 -0500

    Use new ApplicationConfig bootstrap method

---
+++ src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig } from '@angular/core';
+import { CustomTitleStrategyService } from './services/custom-title-strategy.service';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TitleStrategy, provideRouter } from '@angular/router';
+
+import { APP_ROUTES } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimations(),
+    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
+    provideRouter(APP_ROUTES),
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,11 @@
-import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, TitleStrategy } from '@angular/router';
+import { enableProdMode } from '@angular/core';
 
 import { environment } from './environments/environment';
-import { APP_ROUTES } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { CustomTitleStrategyService } from './app/services/custom-title-strategy.service';
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideAnimations(),
-    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
-    provideRouter(APP_ROUTES),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
