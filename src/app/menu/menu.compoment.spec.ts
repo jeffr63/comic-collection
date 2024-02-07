e7e1223618738a0f1c7860deb9f140296e3a8d4b
jeffr63 <jeffr63@gmail.com>
2024-02-07 10:25:16 -0600

    Added button to logout

--- src/app/menu/menu.compoment.spec.ts
+++ src/app/menu/menu.compoment.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatDialogHarness } from '@angular/material/dialog/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -10,9 +11,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
+import { of } from 'rxjs';
+
 import { MenuComponent } from './menu.component';
 import { AuthService } from '../shared/services/auth.service';
-import { of } from 'rxjs';
 
 describe('MenuComponent', () => {
   let fixture: ComponentFixture<MenuComponent>;
@@ -43,7 +45,7 @@ describe('MenuComponent', () => {
     expect(component).toBeDefined();
   });
 
-  it('should load harness for dialog', async () => {
+  it('should open a login dialog when login method is called', async () => {
     const authService = TestBed.inject(AuthService);
     spyOn(authService, 'login').and.callThrough();
 
@@ -55,7 +57,22 @@ describe('MenuComponent', () => {
     expect(dialogs.length).toBe(1);
   });
 
-  it('should load the login dialog', async () => {
+  it('should open login dialog when Login button is clicked', async () => {
+    const authService = TestBed.inject(AuthService);
+    spyOn(component, 'login').and.callThrough();
+
+    authService.logout();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const login = await loader.getHarness(MatButtonHarness.with({ text: 'Login' }));
+    login.click();
+
+    expect(await login.getText()).toBe('Login');
+    expect(component.login).toHaveBeenCalled();
+  });
+
+  it('should load the login dialog with title of Login', async () => {
     const authService = TestBed.inject(AuthService);
     spyOn(authService, 'login').and.callThrough();
 
@@ -66,16 +83,20 @@ describe('MenuComponent', () => {
     expect(await dialogs[0].getTitleText()).toBe('Login');
   });
 
-  // it('should default the user name and passwords to last login', async () => {
-  //   const authService = TestBed.inject(AuthService);
-  //   spyOn(authService, 'login').and.callThrough();
+  it('should display two input fields with labels Email Address and Password', async () => {
+    const authService = TestBed.inject(AuthService);
+    spyOn(authService, 'login').and.callThrough();
 
-  //   component.email = 'test';
-  //   component.password = '123';
-  //   component.login();
-  //   spyOn(component.dialogRef, 'afterClosed').and.returnValue(of({ email: 'test', password: '123' }));
-  //   const dialogs = await loader.getAllHarnesses(MatDialogHarness);
+    component.login();
+    spyOn(component.dialogRef, 'afterClosed').and.returnValue(of({ email: 'test', password: '123' }));
+    const dialogs = await loader.getAllHarnesses(MatDialogHarness);
 
-  //   expect(await dialogs[0].getId()).toBe('test');
-  // });
+    expect(document.getElementsByTagName('input').length).toBe(2);
+
+    const email = document.getElementsByTagName('label')[0] as HTMLHeadElement;
+    expect(email.innerText).toEqual('Email Address');
+
+    const password = document.getElementsByTagName('label')[1] as HTMLHeadElement;
+    expect(password.innerText).toEqual('Password');
+  });
 });
